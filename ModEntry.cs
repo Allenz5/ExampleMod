b7202ae27ea48d4d5f094bfb311070d43f22454e
Allenz5 <allen-zhang-@outlook.com>
2026-03-12 21:07:29 -0400

    Mod feature is not working
    I can confirm mod is loaded, but I still get damage. Can you fix the behavior of the mod?

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,9 +1,12 @@
 using Godot;
+using HarmonyLib;
 
 public partial class ModEntry : Node
 {
     public override void _Ready()
     {
+        var harmony = new Harmony("com.example.nodamagemod");
+        harmony.PatchAll();
         GD.Print("[NoDamageMod] Loaded! Player characters will not lose HP.");
     }
 }
