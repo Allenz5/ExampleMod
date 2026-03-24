e01a4ab5d44597e3cdcb95942e17a853b41573bd
Allenz5 <76895714+Allenz5@users.noreply.github.com>
2026-03-24 19:05:31 -0400

    Remove player invincibility feature (#17)
    Remove the player cannot receive any damage feature from the mod. Strip out all related code, hooks, and logic that prevents the player from taking damage.

--- NoDamagePatch.cs
+++ NoDamagePatch.cs
@@ -4,25 +4,6 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.ValueProps;
 
 [HarmonyPatch(typeof(Creature), nameof(Creature.LoseHpInternal))]
-public static class NoDamagePatch
-{
-    static bool Prefix(Creature __instance, ValueProp props, ref DamageResult __result)
-    {
-        if (__instance.Player != null)
-        {
-            __result = new DamageResult(__instance, props)
-            {
-                UnblockedDamage = 0,
-                WasTargetKilled = false,
-                OverkillDamage = 0
-            };
-            return false;
-        }
-        return true;
-    }
-}
-
-[HarmonyPatch(typeof(Creature), nameof(Creature.LoseHpInternal))]
 public static class PoisonDamageTrackingPatch
 {
     static void Postfix(Creature __instance, DamageResult __result)
